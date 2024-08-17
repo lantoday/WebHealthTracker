975bb5cf194429b9b20174d4a83b18bb893455ae
Lan Chen <Lan.Chen@lincolnuni.ac.nz>
2024-08-18 10:06:09 +1200

    Add tooltip for exercise information

--- web-health-tracker/src/app/ui/exercise/steps.tsx
+++ web-health-tracker/src/app/ui/exercise/steps.tsx
@@ -2,13 +2,34 @@
 
 import React, { useState } from "react";
 import AddStepsModal from "@/app/ui/exercise/add-steps-modal";
+import Tooltip from "@/app/ui/shared/tooltip"; // Adjust import path as necessary
 
 export function StepsComponent() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const hasStepsData = false;
+  const tooltipText = `Step count is the number of steps you take throughout the day.
+Pedometers and digital activity trackers can help you determine your step count. These devices count steps for any activity that involves step-like movement, including walking, running, stair-climbing, cross-country skiing, and even movement as you go about your daily chores.`;
 
   return (
     <>
+      <div className="d-flex">
+        <p className="h5">Steps</p>
+        <Tooltip text={tooltipText}>
+          <div className="p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="--bs-primary-border-subtle"
+              className="bi bi-info-square"
+              viewBox="0 0 16 16"
+            >
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+              <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+            </svg>
+          </div>
+        </Tooltip>
+      </div>
       {hasStepsData && <div>show charts</div>}
 
       {!hasStepsData && (
---
+++ web-health-tracker/src/app/ui/shared/tooltip.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { useState } from "react";
+
+const Tooltip: React.FC<{ text: string; children: React.ReactNode }> = ({
+  text,
+  children,
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div
+      className="tooltip-container"
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
+    >
+      {children}
+      {isVisible && <div className="tooltip-text">{text}</div>}
+    </div>
+  );
+};
+
+export default Tooltip;
