6dd84b854edea04be7a7334274b0a3f9cd4aa6ee
Lan Chen <Lan.Chen@lincolnuni.ac.nz>
2024-08-24 20:05:01 +1200

    Use chart js instead of react-chartjs-2 chart.js

--- web-health-tracker/src/app/ui/exercise/steps-default-chart.tsx
+++ web-health-tracker/src/app/ui/exercise/steps-default-chart.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { Line, Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  PointElement,
-  LineElement,
-} from "chart.js";
+import React, { useRef, useEffect } from "react";
+import Chart from "chart.js/auto";
 import { StepArray } from "@/app/lib/definitions";
 
 interface StepsDefaultChartProps {
@@ -19,42 +11,59 @@ interface StepsDefaultChartProps {
 export function StepsDefaultChartComponent({
   rawData,
 }: StepsDefaultChartProps) {
-  //register the chartjs plugins
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Tooltip
-  );
+  const chartRef = useRef<Chart | null>(null); // Ref to store the chart instance
+
+  // Define the start date for the last 30 days
+  const startDate = new Date();
+  startDate.setDate(new Date().getDate() - 30);
 
   const sortedData = rawData
     ? rawData
         .slice()
         .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+        .filter((entry) => new Date(entry.date) >= startDate)
     : [];
-  const labels = sortedData.map((entry) => entry.date);
-  const data = rawData ? rawData.map((entry) => entry.steps) : [];
 
-  const stepsData = {
-    labels: labels,
-    datasets: [
-      {
-        label: "GeeksforGeeks Line Chart",
-        data: data,
-        fill: false,
-        borderColor: "rgb(75, 192, 192)",
-        borderWidth: 1,
-        pointRadius: 4,
-      },
-    ],
-  };
+  useEffect(() => {
+    // Destroy the previous chart instance if it exists
+    if (chartRef.current) {
+      chartRef.current.destroy();
+    }
+
+    // Initialize the chart
+    const ctx = document.getElementById(
+      "dailyLineChart"
+    ) as HTMLCanvasElement | null;
+
+    if (ctx) {
+      chartRef.current = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: sortedData.map((entry) => entry.date),
+          datasets: [
+            {
+              label: "Step counts (last 30 days)",
+              data: sortedData.map((entry) => entry.steps),
+              fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              borderWidth: 1,
+              pointRadius: 2,
+            },
+          ],
+        },
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [rawData]);
 
   return (
     <>
-      <div>
-        <Line data={stepsData} />
-      </div>
+      <canvas
+        className="my-4 w-100"
+        id="dailyLineChart"
+        width="900"
+        height="380"
+      ></canvas>
     </>
   );
 }
