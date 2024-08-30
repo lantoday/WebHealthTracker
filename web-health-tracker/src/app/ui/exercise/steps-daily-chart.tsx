d06dd24500ea821996a32d6142cb25acc870766b
Lan Chen <Lan.Chen@lincolnuni.ac.nz>
2024-08-30 20:04:48 +1200

    Add option allow user to select line chart or bar chart

--- web-health-tracker/src/app/ui/exercise/steps-daily-chart.tsx
+++ web-health-tracker/src/app/ui/exercise/steps-daily-chart.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Chart from "chart.js/auto";
 import { StepArray } from "@/app/lib/definitions";
 
@@ -10,6 +10,7 @@ interface StepsDefaultChartProps {
 
 export function StepsDailyChartComponent({ rawData }: StepsDefaultChartProps) {
   const chartRef = useRef<Chart | null>(null); // Ref to store the chart instance
+  const [chartType, setChartType] = useState("line");
 
   // Define the start date for the last 30 days
   const startDate = new Date();
@@ -35,15 +36,16 @@ export function StepsDailyChartComponent({ rawData }: StepsDefaultChartProps) {
 
     if (ctx) {
       chartRef.current = new Chart(ctx, {
-        type: "line",
+        type: chartType,
         data: {
           labels: sortedData.map((entry) => entry.date),
           datasets: [
             {
-              label: "Step counts (last 30 days)",
+              label: "Daily step counts (last 30 days)",
               data: sortedData.map((entry) => entry.steps),
               fill: false,
               borderColor: "#0d6efd",
+              backgroundColor: "#0d6efd",
               borderWidth: 1,
               pointRadius: 2,
             },
@@ -52,12 +54,40 @@ export function StepsDailyChartComponent({ rawData }: StepsDefaultChartProps) {
       });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [rawData]);
+  }, [chartType, rawData]);
 
   return (
     <>
+      <div className="d-flex justify-content-center">
+        <div className="form-check m-2">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="chartType"
+            id="radioLineChart"
+            checked={chartType === "line"}
+            onChange={() => setChartType("line")}
+          />
+          <label className="form-check-label" htmlFor="radioLineChart">
+            Line chart
+          </label>
+        </div>
+        <div className="form-check m-2">
+          <input
+            className="form-check-input"
+            type="radio"
+            name="chartType"
+            id="radioBarChart"
+            checked={chartType === "bar"}
+            onChange={() => setChartType("bar")}
+          />
+          <label className="form-check-label" htmlFor="radioBarChart">
+            Bar chart
+          </label>
+        </div>
+      </div>
       <canvas
-        className="my-4 w-100"
+        className="w-100"
         id="dailyLineChart"
         width="900"
         height="380"
