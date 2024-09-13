8b85163d8b9a2561aa7467b1ef8d0efb20c12327
Lan Chen <Lan.Chen@lincolnuni.ac.nz>
2024-09-13 21:03:05 +1200

    Show big image in a modal when user click the thrumbnails

--- web-health-tracker/src/app/ui/history/history-item.tsx
+++ web-health-tracker/src/app/ui/history/history-item.tsx
@@ -1,16 +1,59 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import { HistoryEntry } from "@/app/lib/utils/definitions";
+import ImageModal from "./image-modal";
 
 interface HistoryItemProps {
   data: HistoryEntry | null;
 }
 
-const HistoryItem: React.FC<HistoryItemProps> = (props) => {
+const HistoryItem: React.FC<HistoryItemProps> = ({ data }) => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (!isModalOpen) {
+      setSelectedImage(null);
+    }
+  }, [isModalOpen]);
+
   return (
     <li className="timeline-item mb-5">
-      <h5 className="fw-bold">{props.data?.title}</h5>
-      <p className="text-muted mb-2 fw-bold">{props.data?.date}</p>
-      <p className="text-muted">{props.data?.details}</p>
+      <h5 className="fw-bold">{data?.title}</h5>
+      <p className="text-muted mb-2 fw-bold">{data?.date}</p>
+      <p className="text-muted">{data?.details}</p>
+      <div>
+        {data?.files.map((base64Image, index) => (
+          <img
+            key={index}
+            src={`data:image/jpeg;base64,${base64Image}`}
+            alt={`Thumbnail ${index}`}
+            className="thumbnail"
+            onClick={() =>
+              handleImageClick(`data:image/jpeg;base64,${base64Image}`)
+            }
+            style={{
+              cursor: "pointer",
+              width: "100px",
+              height: "auto",
+              marginRight: "10px",
+            }}
+          />
+        ))}
+      </div>
+
+      {isModalOpen && (
+        <ImageModal imageUrl={selectedImage} onClose={handleCloseModal} />
+      )}
     </li>
   );
 };
---
+++ web-health-tracker/src/app/ui/history/image-modal.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+interface ImageModalProps {
+  imageUrl: string | null;
+  // imageName: string | null;
+  onClose: () => void;
+}
+
+export function ImageModal({ imageUrl, onClose }: ImageModalProps) {
+  return (
+    <>
+      {!imageUrl && <p>There is no image to show</p>}
+
+      {imageUrl && (
+        <div className="modal show d-block" tabIndex={-1} role="dialog">
+          <div
+            className="modal-dialog modal-dialog-centered modal-xl"
+            role="document"
+          >
+            <div className="modal-content">
+              <div className="modal-header">
+                {/* <h5 className="modal-title">Image name</h5> */}
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={onClose}
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <img src={imageUrl} alt="Selected" className="w-100" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ImageModal;
