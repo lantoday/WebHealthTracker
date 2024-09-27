ea9e3bc45c72a39162762e8df169ac31fb92ba3c
Lan Chen <Lan.Chen@lincolnuni.ac.nz>
2024-09-27 15:31:21 +1200

    Add publish yml file for deploy website to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/WebHealthTracker",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
